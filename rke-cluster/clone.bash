882c9e4aa19a98158d284546e475eb1b967f7ab1
seubpong.mon <pjame.fb@gmail.com>
2021-09-14 11:45:21 +0700

    Added rke-cluster

---
+++ rke-cluster/clone.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERSION=v0.0.8
+
+sudo docker run \
+-v $(pwd)/rke-cluster:/wip/output \
+-v ${HOME}/.ssh/:/root/.ssh/ \
+-e IASC_VCS_MODE=git \
+-e IASC_VCS_URL='https://github.com/its-software-services-customers/yru-openedx-infra-nonprod.git' \
+-e IASC_VCS_REF=develop \
+-e IASC_VCS_FOLDER=rke-cluster \
+-it gcr.io/its-artifact-commons/iasc:${VERSION} \
+init
+
+sudo chown -R yruadmin:yruadmin rke-cluster
