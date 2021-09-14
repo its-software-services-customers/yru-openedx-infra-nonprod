a526538098751d74029aab8db18b2616b5c7ec65
seubpong.mon <pjame.fb@gmail.com>
2021-09-15 05:12:23 +0700

    Moved bucket name to vault

--- rke-cluster/00-misc/clone.bash
+++ rke-cluster/00-misc/clone.bash
@@ -2,6 +2,8 @@
 
 VERSION=v0.0.8
 
+VAULT_FILE=gs://<change-me-here>/nonprod/secrets-nonprod.txt
+
 sudo docker run \
 -v $(pwd)/rke-cluster:/wip/output \
 -v ${HOME}/.config/gcloud:/root/.config/gcloud \
@@ -9,7 +11,7 @@ sudo docker run \
 -e IASC_VCS_URL='https://github.com/its-software-services-customers/yru-openedx-infra-nonprod.git' \
 -e IASC_VCS_REF=develop \
 -e IASC_VCS_FOLDER=rke-cluster \
--e IASC_VAULT_SECRETS=gs://yru-openedx-vault/nonprod/secrets-nonprod.txt \
+-e IASC_VAULT_SECRETS=${VAULT_FILE} \
 -it gcr.io/its-artifact-commons/iasc:${VERSION} \
 init
 
