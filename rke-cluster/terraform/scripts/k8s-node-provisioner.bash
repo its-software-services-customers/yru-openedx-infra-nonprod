4076b5dfb5ceda6d9a52a19842660e51f5479cf9
seubpong.mon <pjame.fb@gmail.com>
2021-09-17 06:52:10 +0700

    Setup NTP

--- rke-cluster/terraform/scripts/k8s-node-provisioner.bash
+++ rke-cluster/terraform/scripts/k8s-node-provisioner.bash
@@ -4,7 +4,7 @@ PASSWD=$2
 USER=its
 SSH_KEY=$1
 HOME_DIR="/home/${USER}"
-NTP_SERVER=10.10.2.5
+NTP_SERVER=time.yru.ac.th
 NTP_CONFIG=/etc/ntp.conf
 
 echo ${PASSWD} | sudo -S ls -lrt
@@ -22,10 +22,6 @@ sudo chown ${USER}:sudo -R ${HOME_DIR}/.ssh
 sudo chown ${USER}:sudo ${HOME_DIR}
 sudo chmod 700 ${HOME_DIR}/.ssh
 sudo chmod 600 ${HOME_DIR}/.ssh/authorized_keys
-
-#sudo systemctl start docker
-#sudo systemctl enable docker
-#sudo chown root:docker /var/run/docker.sock
 
 sudo chmod 666 ${NTP_CONFIG}
 sudo echo "server ${NTP_SERVER} prefer" >> ${NTP_CONFIG}
